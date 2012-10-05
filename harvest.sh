ecdaad65a4801c660a9ce1b3144d708ddef74fa7
Peter Binkley <peter.binkley@ualberta.ca>
2012-10-05 17:42:55 -0600

    fixed harvest

--- harvest.sh
+++ harvest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-wget -r -w 5 --convert-links -p -Pharvest -nH -nd -np  "http://wayback.archive-it.org/2217/20101208172948/http://www.albertasource.ca/placenames/resources/searchcontent.php?bt_search=Search&category_id=3&book=1"
+wget -l3 -w 5 --convert-links -Pharvest -nH -nd -np -r  "http://wayback.archive-it.org/2217/20101208172948/http://www.albertasource.ca/placenames/resources/searchcontent.php?bt_search=Search&category_id=3&book=1"
